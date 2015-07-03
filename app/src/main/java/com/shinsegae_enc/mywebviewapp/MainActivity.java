f0bb89eed0983be979bafcaea3a5269c67154a65
mircha1 <mircha@shinsegae.com>
2015-07-03 10:59:16 +0900

    메뉴클릭 제거

--- app/src/main/java/com/shinsegae_enc/mywebviewapp/MainActivity.java
+++ app/src/main/java/com/shinsegae_enc/mywebviewapp/MainActivity.java
@@ -50,7 +50,8 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        //getMenuInflater().inflate(R.menu.menu_main, menu);   --메뉴버튼 제거
         return true;
     }
 
